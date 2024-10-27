bd6764e0906f0f63ee1791ccce210c0926053413
kimtree24 <ktj503@naver.com>
2024-10-27 20:14:10 +0900

    FEAT: SEO 적용 완료 - 100

--- Next.js_prac/week26-2/src/app/page.tsx
+++ Next.js_prac/week26-2/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <title>홈</title>
+        <meta name="description" content="Next.js와 PocketBase 활용, SEO 적용 테스트" />
+        <meta name="keywords" content="Next.js, PocketBase, Note, Home, SEO, likelion" />
       <h1>week26 - next.js 13 실습하기</h1>
       <p>포켓베이스 어캐하는 겁니까...</p>
     </div>
